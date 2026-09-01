29613a0e9210f3399a70ec775d83e21668982790
Arya Shashwat Singh <arya.singh@fareye.com>
2026-09-02 03:18:03 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/minilogistics/MiniLogisticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.minilogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniLogisticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniLogisticsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minilogistics/MiniLogisticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minilogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniLogisticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
